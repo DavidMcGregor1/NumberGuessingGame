baf6e72e42fd67184872f98c9a137f167703c66f
David.McGregor <David.McGregor@hl.co.uk>
2023-09-04 16:43:19 +0100

    added checkGuess function

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ const message = document.getElementById("message");
 const output = document.getElementById("output");
 
 let attempts = 0;
+
+guessSubmit.addEventListener("click", checkGuess);
+
+function checkGuess() {
+  const userGuess = parseInt(guessInput.value);
+
+  if (isNaN(userGuess) || userGuess < 1 || userGuess > 100) {
+    message.textContent = "Please enter a valid number between 1 and 100.";
+    message.style.color = "red";
+    return;
+  }
+  attempts++;
+
+  if (userGuess === randomNumber) {
+    message.textContent = `Congratulations! You guessed the number ${randomNumber} in ${attempts} attempts.`;
+    message.style.color = "green";
+    guessInput.disabled = true;
+    guessSubmit.disabled = true;
+  } else if (userGuess < randomNumber) {
+    message.textContent = "Try higher.";
+    message.style.color = "blue";
+  } else {
+    message.textContent = "Try lower.";
+    message.style.color = "orange";
+  }
+
+  guessInput.value = "";
+  guessInput.focus();
+}
